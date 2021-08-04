e80a5fc71fa13a73c29842df9412592d7abd111d
志诚 李 <1404204769@qq.com>
2021-08-04 10:54:40 +0800

    规范传参，简化接口

--- User.h
+++ User.h
@@ -8,24 +8,22 @@ class CUser
 {
 public:
 	CUser()=delete;
-	CUser(long long int _i64Id, std::string _strAccount, std::string _strTime, std::string _strName, long long int _i64Exp, unsigned int _unLev);
+	CUser(const long long int _i64Id, const std::string& _strAccount, const std::string& _strTime, const std::string& _strName, const long long int _i64Exp, const unsigned int _unLev);
 	~CUser();/*当用户退出时调用此析构函数，并调用CDB::UpdateData函数将数据保存在数据库中*/
 	std::string& getAccount();
-	void setAccount(std::string _strAccount);
 	std::string& getCreateTime();
 	std::string& getName();
-	void setName(std::string _strName);
+	void setName(const std::string& _strName);
 	long long int getId();
-	void setId(long long int _i64Id);
 	long long int getExp();
-	void setExp(long long int _i64Exp);
+	void setExp(const long long int _i64Exp);
 	unsigned int getLev();
-	void setLev(unsigned int _unLev);
+	void setLev(const unsigned int _unLev);
 
 private:
-	unsigned int m_unLev;
-	long long int m_i64Id, m_i64Exp;
-	std::string m_strAccount, m_strCreateTime, m_strName;
+	unsigned int m_unLev=0;
+	long long int m_i64Id=0, m_i64Exp=0;
+	std::string m_strAccount="wrong", m_strCreateTime="now()", m_strName = "wrong";
 };
 
 #endif //!_USER_H_
